2c32da1baeb3693e36b67adaad4def63733a6959
mrpaflex <ukosaviour21@gmail.com>
2023-11-04 06:53:11 +0100

    nestjs mongodb product project

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { UserModule } from './user/user.module';
+import { ProductModule } from './product/product.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+@Module({
+
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    MongooseModule.forRootAsync({
+      useFactory: async (configService: ConfigService)=>({
+        uri: configService.get('MONGODB_URI')
+      }),
+      inject: [ConfigService]
+    }),
+    UserModule, ProductModule, AuthModule],
+})
+export class AppModule {}
---
+++ src/user/model/user.model.ts
@@ -0,0 +1,19 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+@Schema()
+export class User extends Document{
+
+    @Prop()
+    email: string;
+
+    @Prop()
+    name: string;
+
+    @Prop()
+    password: string;
+
+}
+
+export const UserSchema = SchemaFactory.createForClass(User)
+
